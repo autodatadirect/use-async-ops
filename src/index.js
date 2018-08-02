09d27f404135e9c66b94fd4579e2e9746a4f9849
Alexander Lemkin <alemkin@add123.com>
2018-08-02 10:23:18 -0400

    Added standard js to project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import mock from './mock'
 import * as actions from './actions'
 import * as actionTypes from './actionTypes'
 import saga from './saga'
-import {get as getOperation, register} from './operations'
+import { get as getOperation, register } from './operations'
 import { isAsyncOperation, isAsyncComplete, isAsyncFailure } from './helpers'
 
 window.ASYNC_OPS = {
