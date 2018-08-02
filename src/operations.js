521792976e5843d059e72e77f2931be4f01e6e43
Alexander Lemkin <alemkin@add123.com>
2018-08-02 11:47:03 -0400

    #4 Added error checks for registering services

--- src/operations.js
+++ src/operations.js
@@ -4,6 +4,9 @@ const operations = {}
 const mocks = {}
 
 export const register = (name, operation, mock) => {
+  if (!name) throw new Error('Unable to register: No service name has been provided')
+  if (!operation) throw new Error('Unable to register: No operation has been provided for: ' + name)
+  if (operations[name]) console.log('WARNING: Overwriting existing service for: ' + name)
   operations[name] = operation
   mocks[name] = mock
 }
