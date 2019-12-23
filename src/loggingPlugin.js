e573ef94c6d9e4f28eca9067db726b619048a5be
Charles Howard <cdhoward@add123.com>
2019-12-23 15:33:28 -0500

    minor plugin and logging improvements

--- src/loggingPlugin.js
+++ src/loggingPlugin.js
@@ -1,18 +1,43 @@
 
-const log = ({ id, event, name, args, error, result }) => {
-  let s = '%c[ASYNC_OP ' + id + ']'
-  s += ' %c(' + event + ')'
-  s += ' %c' + name
-  s += '%c(' + JSON.stringify(args) + ')'
+const CSS_HEADER = 'color: #777;'
+const CSS_NAME = 'color: #000; font-weight: bold;'
+const CSS_ARGS = 'font-weight: normal;'
+const CSS_EVENT_COMPLETE = 'color: #272;'
+const CSS_EVENT_ERROR = 'color: #c22;'
+const CSS_EVENT_START = 'color: #22c;'
 
-  let eventColor = '#22c'
-  if (error) {
-    eventColor = '#c22'
-  } else if (result) {
-    eventColor = '#272'
+const log = ({ id, event, name, args, error, result }) => {
+  let s = ''
+  const logParams = []
+
+  const append = (css, type, value) => {
+    s += ''
+    if (css) {
+      s += '%c'
+      logParams.push(css)
+    }
+    s += type + ' '
+    logParams.push(value)
   }
 
-  console.log(s, 'color: #777;', 'color: ' + eventColor + ';', 'color: #000; font-weight: bold', 'font-weight: normal')
+  append(CSS_HEADER, '%s', 'ASYNC_OP')
+  append(null, '%i', id)
+  append(CSS_NAME, '%s', name)
+
+  if (event === 'COMPLETE') {
+    append(CSS_EVENT_COMPLETE, '%s', event)
+  } else if (event === 'ERROR') {
+    append(CSS_EVENT_ERROR, '%s', event)
+  } else {
+    append(CSS_EVENT_START, '%s', event)
+  }
+
+  append(CSS_ARGS, '%o', args)
+
+  if (error) append(null, '%o', error)
+  if (result) append(null, '%o', result)
+
+  console.log(s, ...logParams)
 }
 
 let idInc = 0
