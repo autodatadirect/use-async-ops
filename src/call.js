468f7e9d9c0b57f04a752aebdec0298249252c2b
Charles Howard <cdhoward@add123.com>
2019-12-23 12:58:02 -0500

    removing the unneeded clone calls

--- src/call.js
+++ src/call.js
@@ -1,10 +1,8 @@
 import { get } from './registry'
 import plugins from './plugins'
 
-const clone = o => JSON.parse(JSON.stringify(o))
-
 export default (name, ...args) => {
-  const pluginSessions = plugins.map(p => p(name, ...clone(args)))
+  const pluginSessions = plugins.map(p => p(name, ...args))
   const result = get(name)(...args)
   result.then(result => pluginSessions.forEach(p => p.onComplete(result))).catch(e => console.log('error in use-async-ops plugin', e))
   result.catch(error => pluginSessions.forEach(p => p.onError(error)))
