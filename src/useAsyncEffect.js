0975350635cc855964d998607d21bafbedd6ed3a
Charles Howard <cdhoward@add123.com>
2021-03-10 15:54:52 -0500

    Use effect update (#22)
    * updating react and babel
    
    * 1.2.1
    
    * npm update
    
    * useAsyncEffect updated to use useEffect
    
    * 1.2.2

--- src/useAsyncEffect.js
+++ src/useAsyncEffect.js
@@ -1,18 +1,12 @@
-import { useRef } from 'react'
+import { useEffect } from 'react'
 import useAsyncOp from './useAsyncOp'
 
-const hasChanged = (lastValue, thisValue) => JSON.stringify(lastValue) !== JSON.stringify(thisValue)
-
-const clone = o => JSON.parse(JSON.stringify(o))
-
-export default ({ name, options }, ...args) => {
-  const callRef = useRef()
+const useAsyncEffect = ({ name, options }, ...args) => {
   const { call, loading, error, result } = useAsyncOp({ name, options })
-
-  if (!callRef.current || hasChanged(callRef.current, { name, args, options })) {
-    callRef.current = { name, args: clone(args), options }
+  useEffect(() => {
     call(...args)
-  }
-
+  }, [call, ...args])
   return { loading, error, result }
 }
+
+export default useAsyncEffect
